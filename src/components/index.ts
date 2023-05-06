d3213dd73976049ea48501228c4def9210fa1619
Diego Vargas <diegovs0504@gmail.com>
2023-05-06 19:26:50 -0400

    Components Export

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import { Pokemon } from "./Pokemon/Pokemon";
+import { PokemonInfo } from "./PokemonInfo/PokemonInfo";
+import { SelectBar } from "./SelectBar/SelectBar";
+
+
+export {
+    Pokemon,
+    PokemonInfo,
+    SelectBar
+}
