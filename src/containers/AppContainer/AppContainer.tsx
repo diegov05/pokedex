26a694be73e1634be37ed180fe0a0f5efa05c9a2
Diego Vargas <diegovs0504@gmail.com>
2023-05-06 18:39:24 -0400

    Finished AppContainer

--- src/containers/AppContainer/AppContainer.tsx
+++ src/containers/AppContainer/AppContainer.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const AppContainer: React.FC = () => {
     return (
-        <div>
-            <img src="https://wallpaperaccess.com/full/45634.png" alt="" />
+        <div className='overflow-y-hidden h-[100vh] w-[100vw]'>
+            <img className='w-max' src="https://wallpaperaccess.com/full/45634.png" />
+            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-zinc-900 opacity-90 w-[80vw] h-[90vh]'>
+
+            </div>
         </div>
     );
 }
