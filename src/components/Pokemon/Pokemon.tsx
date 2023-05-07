f78e2701d5eea6dd06a013afe64856351b9a1775
Diego Vargas <diegovs0504@gmail.com>
2023-05-06 21:01:48 -0400

    All Responsiveness Done

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -20,8 +20,6 @@ const Pokemon: React.FC = () => {
         return <div>Loading...</div>
     }
 
-    console.log(pokemon)
-
     return (
         <>
             <div className='flex flex-col justify-center items-center'>
@@ -29,7 +27,7 @@ const Pokemon: React.FC = () => {
                 <img className='w-24 md:w-56 lg:w-48 xl:w-56' src={`${pokemon.sprites.other["official-artwork"].front_default}`} alt={pokemon.name} />
                 <div className='w-full'>
                     <div className='flex flex-col md:flex-row lg:flex-row xl:flex-row justify-center items-center gap-2'>
-                        <span className='text-white font-bold uppercase text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>Type</span>
+                        <span className='text-white font-black uppercase text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'>Type</span>
                         <div className='flex flex-row gap-2'>
                             {pokemon.types.map((type) => (
                                 <button className={`flex flex-col justify-center items-center font-black text-white text-[10px] uppercase ${type.type.name}-type px-2 sm:text-[10px] md:text-base lg:text-base xl:text-base md:px-2 md:py-1 lg:px-4 lg:py-2 xl:px-8 rounded-full`}>
--- src/components/PokemonInfo/PokemonInfo.tsx
+++ src/components/PokemonInfo/PokemonInfo.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Pokemon } from '../../interfaces/interfaces';
+import axios from 'axios';
 
 const PokemonInfo: React.FC = () => {
-    return (
-        <div>
 
-        </div>
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null)
+
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            const response = await axios.get("https://pokeapi.co/api/v2/pokemon/1")
+            setPokemon(response.data)
+        }
+        fetchPokemon()
+    }, [])
+
+    if (!pokemon) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <>
+            <div className='flex flex-col gap-8 w-full h-full'>
+                <div className='flex flex-row justify-between w-ful'>
+                    <div>
+                        <h1 className='text-white font-black uppercase text-xs sm:text-sm md:text-base lg:text-base xl:text-base'>Weight</h1>
+                        <p className='text-white font-normal text-xs sm:text-sm md:text-base lg:text-base xl:text-base'>{pokemon.weight}kg</p>
+                    </div>
+                    <div>
+                        <h1 className='text-white font-black uppercase text-xs sm:text-sm md:text-base lg:text-base xl:text-base'>Height</h1>
+                        <p className='text-white font-normal text-xs sm:text-sm md:text-base lg:text-base xl:text-base'>{pokemon.height}m</p>
+                    </div>
+                </div>
+                <div className='flex flex-row justify-between w-ful'>
+                    <div>
+                        <h1 className='text-white font-black uppercase text-xs sm:text-sm md:text-base lg:text-base xl:text-base'>Abilities</h1>
+                        <ul>{pokemon.abilities.map((ability) => (
+                            <li className='text-white font-normal capitalize text-xs sm:text-sm md:text-base lg:text-base xl:text-base'>- {ability.ability.name}</li>
+                        ))}</ul>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
